d99256ba0b3ae4d5564f87cccea3c6761f6aee1e
Denis Zapara <dzapara24@gmail.com>
2021-07-27 11:12:44 +0300

    add homework

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
+import Notiflix from "notiflix";
+import FetchImgAPI from "./js/fetch-img";
+import imgCardTpl from "./templates/image-card.hbs";
 import './sass/main.scss';
+
+const formEl = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more')
+const imagesGallery =document.querySelector('.gallery')
+
+formEl.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+const fetchImgAPI = new FetchImgAPI();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const query = evt.currentTarget.elements.searchQuery.value;
+
+  fetchImgAPI.searchQuery = query;
+
+  if (fetchImgAPI.searchQuery.trim() === '') {
+    return Notiflix.Notify.warning('Write something');
+  };
+  
+  fetchImgAPI.resetPage();
+
+  clearMarkup();
+
+  invisibleBtnLoadMore();
+
+  fetchImgAPI.fetchImg().then(img => {
+    if (img.length === 0) {
+      return Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
+    }
+
+    addVisibleBtnLoadMore();
+
+    addImagesMurkup(img)
+  }).catch();
+}
+
+function onLoadMoreBtn() {
+  if (fetchImgAPI.page > fetchImgAPI.maxPage) {
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+
+    invisibleBtnLoadMore();
+    return
+    }
+
+  fetchImgAPI.fetchImg().then(img => addImagesMurkup(img)).catch();
+}
+
+function addImagesMurkup(images) {
+  const murkup = imgCardTpl(images);
+
+  imagesGallery.insertAdjacentHTML('beforeend', murkup)
+};
+
+function clearMarkup() {
+  imagesGallery.innerHTML = '';
+}
+
+function addVisibleBtnLoadMore() {
+  loadMoreBtn.classList.remove('invisible');
+}
+
+function invisibleBtnLoadMore() {
+  loadMoreBtn.classList.add('invisible');
+}
---
+++ src/js/fetch-img.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const MY_KEY = '22651071-f9099fec57830f45a4a02f8b5';
+const BASE_URL = 'https://pixabay.com/api';
+
+export default class FetchImgAPI {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.maxPage = Math.round(500 / 40);
+  }
+
+   fetchImg() {
+    const urlParams = `key=${MY_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+    const url = `${BASE_URL}/?${urlParams}`;
+
+    this.incrementPage();
+
+    return axios.get(url).then(function (response) {
+  
+      return response.data.hits; 
+    })
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
